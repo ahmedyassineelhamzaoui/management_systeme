76aa88ab53a59cd51ca21703f9e428f3b4e5744c
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-04-16 16:02:20 +0000

    group all operation in product view in a menu

--- resources/views/pages/products.blade.php
+++ resources/views/pages/products.blade.php
@@ -11,15 +11,19 @@
         <main id="main-page"> 
             <section id="my-section">
                 <div>
-                    <div class="flex w-full justify-end">
-                        <button data-modal-target="import-product" data-modal-toggle="import-product" class="block w-full md:w-auto text-white mr-2 bg-green-600 hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center " type="button">
+                    <div class="flex justify-end">
+                       <button id="listOf-operations" data-dropdown-toggle="opperations" class=" font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center button-operation" type="button">list des opération <svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
+                    </div>
+                    <!-- Dropdown menu -->
+                    <div id="opperations" class="z-10 hidden divide-y list-operation rounded-b-lg shadow w-44 ">
+                        <button data-modal-target="import-product" data-modal-toggle="import-product" class="block w-full  text-center  py-2 operation-menu" type="button">
                             importer produits
                         </button>
-                        <a class="text-white px-4 py-1 flex items-center rounded-lg mr-2 bg-red-500" href="{{ route('product.export') }}">Exporter les produits</a>
-                        <button data-modal-target="large-modal" data-modal-toggle="large-modal" class="block w-full md:w-auto text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center " type="button">
+                        <a class="w-full flex justify-center operation-menu py-2" href="{{ route('product.export') }}">Exporter les produits</a>
+                        <button data-modal-target="large-modal" data-modal-toggle="large-modal" class="block w-full text-center operation-menu  py-2" type="button">
                             Ajouter Produit
                         </button>
-                        <button id="alementer-stock"  class="block w-full ml-2 md:w-auto text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center " type="button">
+                        <button id="alementer-stock"  class="block w-full text-center operation-menu  py-2" type="button">
                             Alimenter le stock
                         </button>
                     </div>
