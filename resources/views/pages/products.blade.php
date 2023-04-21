722606fec61cafe91218f628784272eff97297f7
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-04-21 16:28:07 +0000

    fix the probleme whene delete the product

--- resources/views/pages/products.blade.php
+++ resources/views/pages/products.blade.php
@@ -152,7 +152,7 @@
                                                 </svg>                                                      
                                             </button>
                                               
-                                            <button onclick="deleteProduct({{ $product->id }})" data-modal-target="delete-product" data-modal-toggle="delete-product"  class="text-red-600 cursor-pointer delete-product">
+                                            <button onclick="deleteProduct({{ $product->id }})" data-modal-target="delete-product"   class="text-red-600 cursor-pointer delete-product">
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                                     <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                                                 </svg>                                                      
@@ -363,15 +363,15 @@
             </button>
         </div>
         <div class="mt-4">
-            <p>Voulez-vous vraiment supprimer cet utilisateur ?</p>
+            <p>Voulez-vous vraiment supprimer ce Produit ?</p>
         </div>
         <div class="flex justify-end mt-4">
             <form method="post" action="{{route('delete.product')}}">
                 @csrf
                 @method('delete')
                 <input type="hidden" name="product_deletedId" id="product_deletedId">
-                <button data-modal-hide="delete-product" type="button" class="px-4 py-2 rounded-md text-white bg-gray-600 close-modal"  >Cancel</button>
-                <button class="px-4 py-2 rounded-md text-white bg-red-600" type="submit" name="delete-category">Delete</button>
+                <button data-modal-hide="delete-product" type="button" class="px-4 py-2 rounded-md text-white bg-gray-600 close-modal"  >Anuller</button>
+                <button class="px-4 py-2 rounded-md text-white bg-red-600" type="submit" name="delete-category">Supprimer</button>
             </form>
         </div>
     </div>
